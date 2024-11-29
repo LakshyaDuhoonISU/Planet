c8a355b0ab981ddacf2ac06478d146586b416ed6
Lakshya Duhoon <2023.lakshyad@isu.ac.in>
2024-11-29 10:30:14 +0530

    initial commit

---
+++ connectDB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export default function connectDB(){
+    mongoose.connect("mongodb://localhost:27017/Planets")
+    .then(()=>{
+        console.log("Database is connected")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
